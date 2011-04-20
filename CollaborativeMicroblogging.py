20c3c52d06c88abf612c49c40a3e59838738586f
Harshad Joshi <firewalrus@gmail.com>
2011-04-19 22:56:24 -0700

    Edited CollaborativeMicroblogging.py via GitHub

--- CollaborativeMicroblogging.py
+++ CollaborativeMicroblogging.py
@@ -15,6 +15,21 @@
 #              - should ask for it once and store it in the backend.
 #              - This app will make use of sms and mail interface in the next version... 
 
+#This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this package; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+# 02110-1301, USA.
+
 
 
 
