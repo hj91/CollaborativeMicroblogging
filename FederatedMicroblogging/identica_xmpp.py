ec2405a7b9759fc8d2cb3bd9b1811f6cbff5fe62
Harshad Joshi <firewalrus@gmail.com>
2011-04-29 20:56:51 -0700

    Added legal or license stuff...

--- FederatedMicroblogging/identica_xmpp.py
+++ FederatedMicroblogging/identica_xmpp.py
@@ -14,6 +14,23 @@
 #
 # Shouts       - Many thanks to @bavatar for sharing initial code from line 38-42 and 53-57
 
+# All legal or license stuff...in one single line - dont steal code
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this package; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+# 02110-1301, USA.
+
 
 import sys
 
