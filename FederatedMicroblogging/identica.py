dc1e1687241629788d54105283339fbfc3253cee
Harshad Joshi <firewalrus@gmail.com>
2011-06-18 03:19:12 -0700

    Edited FederatedMicroblogging/identica.py via GitHub

--- FederatedMicroblogging/identica.py
+++ FederatedMicroblogging/identica.py
@@ -1,19 +1,36 @@
 #!/usr/bin/env python
 # * coding: utf-8 *
-# (c) Harshad Joshi, 2011
-# http://identi.ca/hj91
+# (c) Harshad Joshi, 18 Jul 2011
+# Little refactoring done
+# ToDo 
+# Give it some error and exception handling and make it as versatile as it can become.
 
 import json, urllib2
 from urllib import urlencode
 import sys,feedparser
 
+def url(a):
+	n=urllib2.urlopen(a)
+	b=feedparser.parse(n)
+	for i in range(len(b.entries)):
+	       	print b.entries[i].title
+	del(n)
+	del(b)
+	
+def url2(a):
+	n=urllib2.urlopen(a)
+	b=feedparser.parse(n)
+	for entry in b.entries:
+		print entry.title, entry.id
+	del(n)
+	del(b)
 
 class IdentiCa:
 	def __init__(self,user,pwd,apibase):
 		self.user = user
 		self.pwd = pwd
 		self.apibase = apibase
-
+		
 
 	def post(self,msg):
 	# connection magic
@@ -29,38 +46,29 @@ class IdentiCa:
 			sys.exit(0)
 		else:
 			if (msg.lower() == 'mentions'):
-				a=urllib2.urlopen("http://identi.ca/index.php/api/statuses/mentions/hj91.rss")
-				b=feedparser.parse(a)
-				for i in range(len(b.entries)):
-	        			print b.entries[i].title
-				del(a)
-				del(b)
+				url("http://identi.ca/index.php/api/statuses/mentions/hj91.rss")
 			else:
-				if (msg.lower() == 'friends'):
-					a=urllib2.urlopen('http://identi.ca/api/statusnet/app/subscriptions/32987.atom')
-					b=feedparser.parse(a)
-					c=[]
-					print 'The list of your friends is'				
-					for entry in b.entries:
-						print entry.title, entry.id
-
-					v=raw_input('Enter the id of friend you want to see updates of ')
-					t=urllib2.urlopen("http://identi.ca/api/statuses/user_timeline/"+v+".atom")
-					l=feedparser.parse(t)
-					for y in l.entries:
-						print y.title
-	
+				if (msg.lower() == 'home'):
+					url("http://identi.ca/api/statuses/friends_timeline/hj91.rss")
 				else:
-					if (msg.lower() == 'status'):
-						e=raw_input("Enter your status")				
-						# url encode it nicely and set your own client name – no links in source!
-						themsg = urlencode({'status':e,'source':'CollaborativeMicroblogging'})
-						# and send the notice
-						urllib2.urlopen(self.apibase+'/statuses/update.json?s', themsg)
+					if (msg.lower() == 'friends'):
+						url2('http://identi.ca/api/statusnet/app/subscriptions/32987.atom')
+						
+						v=raw_input('Enter the id of friend you want to see updates of >> ')
+						url("http://identi.ca/api/statuses/user_timeline/"+v+".atom")
+						
+		
+					else:
+						if (msg.lower() == 'status'):
+							e=raw_input("Enter your status >> ")				
+							# url encode it nicely and set your own client name â€“ no links in source!
+							themsg = urlencode({'status':e,'source':'CollaborativeMicroblogging'})
+							# and send the notice
+							urllib2.urlopen(self.apibase+'/statuses/update.json?s', themsg)
 
-	
 
 m=IdentiCa(user = "hj91",pwd = "yourpasswd ",apibase = "https://identi.ca/api")
 while(1):
-	c=raw_input("Enter mentions, friends or status >> ")
+	c=raw_input ("Enter somthing >> ")
 	m.post(c)
+	
